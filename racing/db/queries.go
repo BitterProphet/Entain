9b9922326922de426c9a3d258f3862b00ef2e092
BitterProphet <46408148+BitterProphet@users.noreply.github.com>
2021-11-10 20:05:50 +1000

    Task 2 query
    added new query to queries.go to filter results to only visible
    
    ++ referenced new query var

--- racing/db/queries.go
+++ racing/db/queries.go
@@ -33,3 +33,18 @@ func getVisibleRaceQueries() map[string]string {
 `,
 	}
 }
+func getRaceQueriesbyTime() map[string]string {
+	return map[string]string{
+		racesList: `
+			SELECT 
+				id, 
+				meeting_id, 
+				name, 
+				number, 
+				visible, 
+				advertised_start_time 
+			FROM races
+			ORDER by advertised_start_time desc		
+`,
+	}
+}
